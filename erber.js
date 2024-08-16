145e55d01b972246c03a41d17e38911aae5ef211
sleakchat <support@sleak.chat>
2024-08-16 14:47:47 +0200

    remove onload event

--- erber.js
+++ erber.js
@@ -13,8 +13,6 @@ function loadCSS(href) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = href;
-    // link.onload = resolve;
-    // link.onerror = reject;
     document.head.appendChild(link);
   });
 }
@@ -115,8 +113,8 @@ function initAnimations() {
     });
     tl.to(targetElement, {
       width: "0%",
-      // duration: 1, // Adjust the duration for smoothness
-      ease: "power2.inOut", // Use easing for smoother animation
+      // duration: 1,
+      ease: "power2.inOut",
     });
   });
 
@@ -125,9 +123,9 @@ function initAnimations() {
     .timeline({
       scrollTrigger: {
         trigger: ".framer-nw2mzd",
-        start: "top bottom", // Adjust this value as needed
-        end: "bottom top", // Adjust this value as needed
-        scrub: 3, // Synchronize the animation with the scrollbar
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 3,
         toggleActions: "play none none none",
       },
     })
@@ -139,15 +137,15 @@ function initAnimations() {
   const carouselItems = document.querySelectorAll(".framer--carousel li");
   gsap.from(carouselItems, {
     scrollTrigger: {
-      trigger: ".framer--carousel", // The element that triggers the animation
-      start: "center bottom", // Adjust based on when you want the animation to start
+      trigger: ".framer--carousel",
+      start: "center bottom",
       end: "bottom center",
-      // scrub: true, // Allows the animation to scrub based on scroll position
+      // scrub: true
     },
     opacity: 0,
-    y: "100px", // Move items from 50px below their original position
-    stagger: 0.15, // Stagger each item by 0.2 seconds
-    duration: 0.4, // Animation duration
+    y: "100px",
+    stagger: 0.15,
+    duration: 0.4,
   });
 
   // process containers
@@ -159,33 +157,29 @@ function initAnimations() {
   );
   gsap.from(processContainers, {
     scrollTrigger: {
-      trigger: processContainersWrapper, // The element that triggers the animation
-      start: "center bottom", // Adjust based on when you want the animation to start
+      trigger: processContainersWrapper,
+      start: "center bottom",
       end: "bottom center",
-      // scrub: true, // Allows the animation to scrub based on scroll position
+      // scrub: true,
     },
     opacity: 0,
-    y: 50, // Move items from 50px below their original position
-    stagger: 0.15, // Stagger each item by 0.2 seconds
-    duration: 0.4, // Animation duration
+    y: 50,
+    stagger: 0.15,
+    duration: 0.4,
   });
 
   setTimeout(() => {
-    // Access the iframe element
     const iframe = document.getElementById("iFrameResizer0");
 
-    // Wait for the iframe to load
-    iframe.onload = function () {
-      // Access the iframe's document
-      const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    // Access the iframe's document
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-      // Create a <style> element
-      const style = document.createElement("style");
-      style.type = "text/css";
-      style.innerHTML = ".wally-main-item { display: none !important; }";
+    // Create a <style> element
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = ".wally-main-item { display: none !important; }";
 
-      // Append the <style> element to the iframe's document head
-      iframeDoc.head.appendChild(style);
-    };
+    // Append the <style> element to the iframe's document head
+    iframeDoc.head.appendChild(style);
   }, 3000);
 }
