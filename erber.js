e9b85140a5dd8b4142bc63bdccf4ac5c89c7643a
sleakchat <support@sleak.chat>
2024-08-22 21:09:56 +0200

    name attribute

--- erber.js
+++ erber.js
@@ -51,7 +51,8 @@ function initAnimations() {
   gsap.registerPlugin(ScrollTrigger);
 
   // header
-  let targetElement = document.querySelector(".framer-1jbc6qn");
+  // let targetElement = document.querySelector(".framer-1jbc6qn");
+  let targetElement = document.querySelector('[data-framer-name=header-wrap"]');
   gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
   let lastScrollTop = 0;
   let threshold = 100;
