5ad8a31e3879ebe94937a7cb3d76bf9e8672b910
sleakchat <support@sleak.chat>
2024-08-23 09:15:38 +0200

    x

--- erber.js
+++ erber.js
@@ -234,7 +234,7 @@ function initAnimations() {
             autoAlpha: 1,
             ease: "power2.inOut",
           });
-        // .to(menuContainer, { duration: 0, autoAlpha: 1 }, "-=0.6");
+        .to(menuContainer, { duration: 0, autoAlpha: 1 }, "-=0.6");
         menuOpenState = true;
       }
     });
