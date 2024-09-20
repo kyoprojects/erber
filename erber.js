ed85978d64366fd3708dfc8e64b21633279f9c7c
sleakchat <support@sleak.chat>
2024-09-20 16:51:18 +0200

    refactor: Update carousel item selector and improve button styling in erber.css

--- erber.js
+++ erber.js
@@ -54,38 +54,38 @@ if (typeof currentUrl === 'undefined') {
     // gsap
     gsap.registerPlugin(ScrollTrigger);
 
-    // header
-    // let targetElement = document.querySelector(".framer-1jbc6qn");
-    let targetElement = document.querySelector('[data-framer-name="header-wrap"]');
-    gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
-    let lastScrollTop = 0;
-    let threshold = 100;
-    let hysteresis = 100;
+    // // header
+    // // let targetElement = document.querySelector(".framer-1jbc6qn");
+    // let targetElement = document.querySelector('[data-framer-name="header-wrap"]');
+    // gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
+    // let lastScrollTop = 0;
+    // let threshold = 100;
+    // let hysteresis = 100;
 
-    window.addEventListener(
-      'scroll',
-      () => {
-        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    // window.addEventListener(
+    //   'scroll',
+    //   () => {
+    //     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (currentScroll > lastScrollTop) {
-          // Downscroll
-          if (currentScroll > threshold + hysteresis) {
-            // Adding hysteresis
-            gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
-          }
-        } else {
-          // Upscroll
-          if (currentScroll > threshold + hysteresis) {
-            gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 1 });
-          } else if (currentScroll <= threshold - hysteresis) {
-            // Using hysteresis
-            gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 0 });
-          }
-        }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-      },
-      false
-    );
+    //     if (currentScroll > lastScrollTop) {
+    //       // Downscroll
+    //       if (currentScroll > threshold + hysteresis) {
+    //         // Adding hysteresis
+    //         gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
+    //       }
+    //     } else {
+    //       // Upscroll
+    //       if (currentScroll > threshold + hysteresis) {
+    //         gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 1 });
+    //       } else if (currentScroll <= threshold - hysteresis) {
+    //         // Using hysteresis
+    //         gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 0 });
+    //       }
+    //     }
+    //     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    //   },
+    //   false
+    // );
 
     // split text
     let textElement = document.querySelectorAll('.framer-jg88uz p');
